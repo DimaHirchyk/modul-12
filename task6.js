d24c931939a5df1be9e3de41cc6e29ea13096cef
Dima Hirchyk <dimzuk04@gmail.com>
2024-11-09 12:19:48 +0200

    task6

--- task6.js
+++ task6.js
@@ -2,9 +2,26 @@
 //   яка приймає рядок str і повертає новий рядок,
 //     у якому з кожного слова видалені всі голосні букви.
 
-const vowels = 'aeiouAEIOU'
+const vowels = "aeiouAEIOU";
+
+function removeVowelsFromWords(str) {
+  let newWord = "";
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+    if (vowels.includes(str[i])) {
+      continue;
+    } else {
+      newWord += str[i];
+    }
+  }
+  return newWord;
+}
 
 // Приклади використання:
-console.log(removeVowelsFromWords('Hello, world!')) // "Hll, wrld!"
-console.log(removeVowelsFromWords('JavaScript is awesome!')) // "JvScrpt s wsm!"
-console.log(removeVowelsFromWords('Remove vowels from words')) // "Rmv vwls frm wrds"
+console.log(removeVowelsFromWords("Hello, world!")); // "Hll, wrld!"
+console.log(
+  removeVowelsFromWords("JavaScript is awesome!")
+); // "JvScrpt s wsm!"
+console.log(
+  removeVowelsFromWords("Remove vowels from words")
+); // "Rmv vwls frm wrds"

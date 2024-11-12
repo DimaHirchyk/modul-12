51c197f82e436b16ea507c83dabc66a088544f45
Mihail <mykh.shev4enko@gmail.com>
2024-11-12 15:45:24 +0200

    "done"

--- task11.js
+++ task11.js
@@ -4,6 +4,23 @@
 // функція повинна повертати null.
 
 // Приклади використання:
-console.log(extractNumber('abc123def')) // 123
-console.log(extractNumber('no numbers here')) // null
-console.log(extractNumber('42 is the answer')) // 42
+
+function extractNumber(str) {
+  let number = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (char >= "0" && char <= "9") {
+      number += char;
+    } else if (number) {
+      return Number(number);
+    }
+  }
+
+  return number ? Number(number) : null;
+}
+
+console.log(extractNumber("abc123def")); // 123
+console.log(extractNumber("no numbers here")); // null
+console.log(extractNumber("42 is the answer")); // 42

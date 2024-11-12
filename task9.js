a6345e65852503274c7c85127c31024489114eef
Mihail <mykh.shev4enko@gmail.com>
2024-11-12 14:23:37 +0200

    done

--- task9.js
+++ task9.js
@@ -1,6 +1,17 @@
-//Дано рядок, який складається із символів.Напишіть функію
-// яка перевіряє, що першим символом цього рядка є літера 'a'.
-//  Якщо це так - вивести "так", у іншому випадку вивести "ні"
+//? Дано рядок, який складається із символів.Напишіть функію
+//? яка перевіряє, що першим символом цього рядка є літера 'a'.
+//? Якщо це так - вивести "так", у іншому випадку вивести "ні"
+function firstSymb(string) {
+  if (string.startsWith("a")) {
+    return "так";
+  } else {
+    return "ні";
+  }
+}
+const string = "abcde"; // тестовий рядок 1
+const stringSecond = "bcde"; // тестовий рядок 2
+const subString = "absolute"; // тестовий рядок 3
 
-const string = 'abcde' // тестовий рядок 1
-const stringSecond = 'bcde' // тестовий рядок 2
+console.log(firstSymb(string)); // так
+console.log(firstSymb(stringSecond)); // ні
+console.log(firstSymb(subString)); // так
